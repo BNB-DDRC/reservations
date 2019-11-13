a7deac13eb56eab6c65805c00a29e1ec3f9dda40
David José Mosher <davidjosemosher@gmail.com>
2019-11-13 00:38:35 -0800

    Refactor CSS and props references

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,22 +6,31 @@ import Date from './Date';
 import Guest from './Guest';
 import ReserveButton from './ReserveButton';
 
-const ResverationsDiv = styled.div`
-  width: 374px;
-  height: 456px;
+const ReservationsDiv = styled.div`
+  width: 325px;
+  height: max-content;
   box-shadow: rgba(0, 0, 0, 0.05) 0px 2px 6px, rgba(0, 0, 0, 0.07) 0px 0px 0px 1px;
   background: rgb(255, 255, 255);
-  border-radius: 3px;
+  border-radius: 1px;
+  padding: 25px;
 `;
+// width: 374px;
+// height: 456px;
 
 const DetailsDiv = styled.div`
   padding-top: 18px;
-  margin-left: 25px;
 `;
 
 const Details = styled.span`
   color: rgb(72,72,72);
   font-family: 'Montserrat', sans-serif;
+`;
+
+const Line = styled.div`
+  height: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border-bottom: 0.5px solid rgb(228, 231, 231);
 `;
 
 class App extends React.Component {
@@ -65,12 +74,12 @@ class App extends React.Component {
     }
 
     return (
-      <ResverationsDiv>
+      <ReservationsDiv>
         <DetailsDiv>
           <div>
             <Details style={{ fontWeight: 'bold', fontSize: '22px' }}>
               $
-            {listingInfo.pricePerNight}
+            {listingInfo.price}
             </Details>
             <Details style={{ fontWeight: '600', fontSize: '12px' }}>
               {'   '}
@@ -82,17 +91,18 @@ class App extends React.Component {
               <img style={{ width: '9px', height: '9px' }}
                 src="data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PScwIDAgMTAwMCAxMDAwJyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIGZpbGw9JyMwMDg0ODknPjxwYXRoIGQ9J005NzIgMzgwYzkgMjggMiA1MC0yMCA2N0w3MjUgNjE5bDg3IDI4MGMxMSAzOS0xOCA3NS01NCA3NS0xMiAwLTIzLTQtMzMtMTJMNDk5IDc5MCAyNzMgOTYyYTU4IDU4IDAgMCAxLTc4LTEyIDUwIDUwIDAgMCAxLTgtNTFsODYtMjc4TDQ2IDQ0N2MtMjEtMTctMjgtMzktMTktNjcgOC0yNCAyOS00MCA1Mi00MGgyODBsODctMjc5YzctMjMgMjgtMzkgNTItMzkgMjUgMCA0NyAxNyA1NCA0MWw4NyAyNzdoMjgwYzI0IDAgNDUgMTYgNTMgNDB6Jy8+PC9zdmc+"
               />
-              {` ${listingInfo.ratings}`}
+              {` ${listingInfo.rating > 5 ? '5.0' : String(listingInfo.rating).concat('.0')}`}
             </Details>
             <Details style={{ fontSize: '12px', color: '#828282' }}>
-              {`  (${listingInfo.reviews} reviews)`}
+              {`  (${listingInfo.review_count} reviews)`}
             </Details>
           </div>
         </DetailsDiv>
+        <Line />
         <Date />
         <Guest />
         <ReserveButton handleSubmit={this.handleSubmit} />
-      </ResverationsDiv>
+      </ReservationsDiv>
     );
   }
 }
