ab13c5f961b86b0595c1152662076da371c34898
David José Mosher <davidjosemosher@gmail.com>
2019-11-13 00:36:36 -0800

    Refactor CSS

--- client/components/Guest.jsx
+++ client/components/Guest.jsx
@@ -12,32 +12,33 @@ const GuestBox = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  width: 316px;
-  height: 40px;
-  margin-left: 24px;
-  margin-right: 24px;
+  width: auto;
+  height: min-content;
   border: 1px solid rgb(228, 231, 231);
   border-radius: 1px;
   font-size: 16px;
   color: rgb(72,72,72);
-  padding-left: 8px;
+  padding-top: 7px;
+  padding-bottom: 7px;
+  padding-left: 10px;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const Guest = () => (
   <GuestDiv>
-    <div style={{
-      marginLeft: '24px', fontSize: '12px', color: 'rgb(72,72,72)', fontWeight: '600',
-    }}
-    >
+    <div style={{ fontSize: '12px', color: 'rgb(72,72,72)', fontWeight: '600' }}>
       Guests
     </div>
     <GuestBox>
-      <div>1 guest</div>
+      1 guest
       <div style={{
-        marginRight: '16px', fontSize: '16px', color: 'rgb(72,72,72)', fontWeight: '600',
+        marginRight: '16px', fontSize: '16px', color: 'rgb(72,72,72)', fontWeight: '200',
       }}
       >
-        v
+        V
       </div>
     </GuestBox>
   </GuestDiv>
